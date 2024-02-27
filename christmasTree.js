cd8feba834f67d16b1308498138eff4b60581aa0
GATA5 <Tangjing.Gan@coop.ch>
2024-02-27 11:40:55 +0100

    Initial Commit

---
+++ christmasTree.js
@@ -0,0 +1,10 @@
+
+let n = 5;
+
+for (let h = 0; h < (1 + 5 * n); h++) {
+    console.log("*****");
+}
+
+for (let i = 0; i < n; i++) {
+    console.log("*");
+}
---
+++ test.js
@@ -0,0 +1,39 @@
+let age = 18;
+
+for (let i = 0;i < age; i++)  {
+    console.log(i);
+}
+
+if (i => 18){
+    console.log(true);
+} else  {
+    console.log(false);
+}
+
+let score = 0;
+
+if (score!==0){
+    console.log("Score is available");
+}
+
+if (score === true){
+    console.log("Score is evaluated as truthy.");
+} else {
+    console.log("Score isn't truthy.");
+}
+
+let username = "";
+
+if (username.length > 0){
+    console.log("Username is available.");
+} else {
+    console.log("Username is evaluated as falsy.");
+}
+
+let isAdmin = false;
+
+if (isAdmin === true){
+    console.log("User is admin.");
+} else {
+    console.log("User not registered as admin.");
+}
